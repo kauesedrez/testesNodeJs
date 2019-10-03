b9690e0b01976ce9d57e4ebe0fa3a16f06838b7e
WINETU KAUE SEDREZ BILHALVA <w.bilhalva@gestor.local>
2019-10-03 18:13:16 -0300

    adicionada as divs fotos futebol

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.post("/carregaInfos", function(request, response) {
 
 	const nome = request.body.nome;
 
-	var html = "<div class='div1'><img src='https://www.globalframe.com.br/gf_base/empresas/MIGA/imagens/4865352110763FE0504CF949B440EC3BEA03_ferias.jpg' class='img-responsive'></div>"
+	var html = "<div class='div1'><img src='https://aventurasnahistoria.uol.com.br/media/_versions/legacy/2018/06/12/futebol-capas-1100594_widelg.png' class='img-responsive'></div>"
+
+	html += "<div class='div1'><img src='https://www.globalframe.com.br/gf_base/empresas/MIGA/imagens/4865352110763FE0504CF949B440EC3BEA03_ferias.jpg' class='img-responsive'></div>"
 
 	html += "<div class='div1'><img src='https://blog.luz.vc/wp-content/uploads/2014/06/o-que-fazer-nas-f%C3%A9rias-de-ver%C3%A3o-696x522.jpg' class='img-responsive'></div>"
 
