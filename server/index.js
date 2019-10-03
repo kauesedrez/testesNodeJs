0bc0b3a5b40b23bba5f1498d2e06afdd524605b9
WINETU KAUE SEDREZ BILHALVA <w.bilhalva@gestor.local>
2019-10-03 16:23:11 -0300

    Commit Inicial

---
+++ server/index.js
@@ -0,0 +1,29 @@
+var bodyParser = require("body-parser");
+var express = require("express");
+var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.set("port", 3000);
+app.use(express.static(__dirname + "/_public")); // diretorio compartilhado entre as paginas do programa
+app.set("views", "./_views");
+app.set("view engine", "ejs");
+var server = app.listen(app.get("port"));
+
+console.log("Online")
+
+app.get("/testeCacheAjax", function(request, response) {
+    let parametros = [];
+    parametros["from"] = "teste";
+    response.render("home/TesteCacheAjax", parametros);
+});
+
+app.post("/carregaInfos", function(request, response) {
+
+	const nome = request.body.nome;
+
+	setTimeout(()=>{
+		response.json(nome);
+	},2000);
+	
+
+});
