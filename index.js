42ff73afeba8d64d77f2436e57675dd2ac47b66c
WINETU KAUE SEDREZ BILHALVA <w.bilhalva@gestor.local>
2019-10-04 13:00:12 -0300

    2 - fontawesome

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-var porta = process.env.PORT || 3200;
+var porta = process.env.PORT || 1201;
 app.set("port", porta);
 app.use(express.static(__dirname + "/_public")); // diretorio compartilhado entre as paginas do programa
 app.set("views", "./_views");
